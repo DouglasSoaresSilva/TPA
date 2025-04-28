9eee1ebb8ecffc844e4cd4f4cbb2ec8e2d2ec64c
Douglas Soares <douglas201003@gmail.com>
2025-04-28 18:02:47 -0300

    Add files via upload

---
+++ Calc Area/Arearetangulo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arearetangulo;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Arearetangulo {
+
+    public static void main(String[] args) {
+        //Calculo da Area do Retângulo
+       double base = 50;
+       double altura = 30;
+       double area = base*altura;
+       System.out.print("Area do retângulo é:" + area);
+    }
+}
---
+++ Calc Area/Calcsubtracao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcsubtracao;
+
+/**Eu, Douglas Soares Silva estou fazendo a atividade no dia 23 de abril
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Calcsubtracao {
+    public static void main(String[] args) {
+        double num1 = 50;
+        double num2 = 99;
+        //Calculo
+        double resultado = num2 - num1;
+        System.out.print("Resultado da subtração é:" + resultado);
+    }
+}
