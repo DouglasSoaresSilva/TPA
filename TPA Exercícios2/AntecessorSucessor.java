2c571d5f41397a92e3cad23852fef0c3f2b5fb28
Douglas Soares <douglas201003@gmail.com>
2025-06-11 13:57:30 -0300

    Update AntecessorSucessor.java

--- TPA Exercícios2/AntecessorSucessor.java
+++ TPA Exercícios2/AntecessorSucessor.java
@@ -4,8 +4,8 @@ public class AntecessorSucessor {
     public static void main(String[] args) {
 
     int num = Integer.parseInt(JOptionPane.showInputDialog("Digite o número: "));
-    int ante = num + 1;
-    int suce = num - 1;
+    int suce = num + 1;
+    int ante = num - 1;
     JOptionPane.showMessageDialog(null, "Antecessor: " + ante);
     JOptionPane.showMessageDialog(null, "Sucessor: " + suce);
  }
